3c09ab72b8a8746129fbcaefb1e2bb7bfda21420
s-gala <galinasavtchenko78@gmail.com>
2025-02-04 14:34:57 +0300

    check 4.3

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -9,7 +9,7 @@ INSTRUCTION = consts.INSTRUCTION_GCD
 
 def get_question_answer():
     first_num, second_num = get_random_number(1, 100), get_random_number(1, 100)
-    random_question = (f'{first_num} {second_num}')
+    random_question = f'{first_num} {second_num}'
     right_answer = str(math.gcd(first_num, second_num))
     return random_question, right_answer
 
