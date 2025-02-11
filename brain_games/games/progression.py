04bdbb86f3e0f5ec2f1b7012e84c2b2ea3d34343
s-gala <galinasavtchenko78@gmail.com>
2025-02-11 21:02:44 +0300

    check 5.2

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,13 +1,14 @@
 from brain_games.consts import PROGRESSION_INSTRUCTION
 from brain_games.engine import game_engine
-from brain_games.utils import get_random_number
+from brain_games.utils import get_random_number as random
 
 INSTRUCTION = PROGRESSION_INSTRUCTION
 
 
 def get_question_answer():
-    progression = [num for num in range(get_random_number(1, 40), 130, get_random_number(1, 11))][:get_random_number(5,11)] 
-    index_hidden_num = get_random_number(0, len(progression) - 1)
+    progression = \
+    [num for num in range(random(1, 40), 130, random(1, 11))][:random(5, 11)] 
+    index_hidden_num = random(0, len(progression) - 1)
     right_answer = progression[index_hidden_num]
     progression[index_hidden_num] = '..'
     return ' '.join(str(num) for num in progression), str(right_answer)
